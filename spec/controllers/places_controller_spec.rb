adf594bb1d90efa019bb6645cf85778997f5676b
candicenoel <candice29@gmail.com>
2019-12-07 22:37:18 +0000

    Added factory bot gem

--- spec/controllers/places_controller_spec.rb
+++ spec/controllers/places_controller_spec.rb
@@ -15,11 +15,7 @@ RSpec.describe PlacesController, type: :controller do
     end
 
     it "should successfully show the new form" do
-      user = User.create(
-        email:                 'fakeuser@gmail.com',
-        password:              'secretPassword',
-        password_confirmation: 'secretPassword'
-      )
+      user = FactoryBot.create(:user)
       sign_in user
 
       get :new
@@ -35,11 +31,7 @@ RSpec.describe PlacesController, type: :controller do
     end
 
     it "should successfully create a new place in our database" do
-      user = User.create(
-        email:                 'fakeuser@gmail.com',
-        password:              'secretPassword',
-        password_confirmation: 'secretPassword'
-      )
+      user = FactoryBot.create(:user)
       sign_in user
 
       post :create, params: { place: { address: '' } }
@@ -51,11 +43,7 @@ RSpec.describe PlacesController, type: :controller do
     end
 
     it "should properly deal with validation errors" do
-      user = User.create(
-        email:                 'fakeuser@gmail.com',
-        password:              'secretPassword',
-        password_confirmation: 'secretPassword'
-      )
+      user = FactoryBot.create(:user)
       sign_in user
 
       place_count = Place.count
