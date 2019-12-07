725782af55a7a263decaac8979c10fc6d8068ea7
candicenoel <candice29@gmail.com>
2019-12-07 07:30:46 +0000

    Adding places to database

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -2,7 +2,18 @@ class PlacesController < ApplicationController
   def new
     @place = Place.new
   end
-  
+
   def index
+  end
+
+  def create
+    @place = Place.create(place_params)
+    redirect_to root_path
+  end
+
+  private
+
+  def place_params
+    params.require(:place).permit(:name)
   end
 end
--- spec/controllers/places_controller_spec.rb
+++ spec/controllers/places_controller_spec.rb
@@ -14,4 +14,14 @@ RSpec.describe PlacesController, type: :controller do
       expect(response).to have_http_status(:success)
     end
   end
+
+  describe "places#create action" do
+    it "should successfully create a new place in our database" do
+      post :create, params: { place: { name: '' } }
+      expect(response).to redirect_to root_path
+
+      place = Place.last
+      expect(place.name).to eq('')
+    end
+  end
 end
